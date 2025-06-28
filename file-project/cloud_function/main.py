969e8613d5165810d1be598ce1d2194c9ebfbc03
Dhruv Patil <dhruv.patil284@gmail.com>
2025-06-28 09:18:19 +0530

    Move Git root to inner folder and add full code

--- file-project/cloud_function/main.py
+++
@@ -1,5 +0,0 @@
-def process_file(event, context):
-    name = event['name']
-    size = event['size']
-    ftype = name.split('.')[-1]
-    print(f"File uploaded: {name}, size: {size} bytes, type: {ftype}")
